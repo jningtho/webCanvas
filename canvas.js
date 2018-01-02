80115683a866cbd28540e7a3009689617b0ff45c
jningtho <jningtho@x-designs.net>
2018-01-02 14:31:49 +0530

    update

--- canvas.js
+++ canvas.js
@@ -45,29 +45,60 @@ var c = canvas.getContext('2d');
   // var dy = (Math.random() - 0.5) * 10;
   // var radius = 30;
 
+  var mouse = {
+      x: undefined,
+      y: undefined
+  }
+  var maxRadius = 40;
+  var minRadius = 2;
+
+//color array for the circles.
+  var colorArrray = [
+    '#A85E0A',
+    '#FFA339',
+    '#F5911E',
+    '#0076A8',
+    '#1EB5F5',
+  ];
+  console.log();
+
+// Event Listener
+  window.addEventListener('mousemove', function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+  })
+
+  window.addEventListener('rezise', function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  })
 // Object Oriented JS function
   function Circle(x, y, dx, dy, radius) {
       this.x = x;
       this.y = y;
-      this.dx = dx;
-      this.dy = dy;
+      this.dx = dx; //xvelocity
+      this.dy = dy; //yvelocity
       this.radius = radius;
+      this.minRadius = minRadius;
+      this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
       //method draw
       this.draw = function() {
           c.beginPath();
           c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false); //properties
-          c.strokeStyle = 'blue';
-          c.stroke();
+          // //c.strokeStyle = 'blue';
+          // c.stroke();
+          // c.fillStyle = colorArray[Math.floor(Math.random() * colorArray.length)];
+          c.fillStyle = this.color;
           c.fill();
       }
-
+      //method update
       this.update = function() {
-        if (this.x + this.radius > innerWidth || x - radius < 0) {
-          this.dx = -this.dx;
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx;
         }
-        if (this.y + this.radius > innerHeight || y - radius < 0) {
-          this.dy = -this.dy;
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy;
         }
 
         this.x += this.dx;
@@ -75,54 +106,47 @@ var c = canvas.getContext('2d');
 
         this.draw();
 
+        //interactivity
+        if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50)
+        {
+            if (this.radius < maxRadius) {
+                this.radius += 1;
+            }
+
+        }   else if (this.radius > this.minRadius) {
+            this.radius -= 1;
+        }
+
+        this.draw();
+
       }
   }
 
-
-
-
-  // create array to store the x number of clicles
+  // create array to store the x number of cirles
   var circleArray = [];
 
-  for (var i = 0; i < 100; i++) {
-      var radius = 30;
-      var x = Math.random() * (innerWidth - radius * 2) + radius;
-      var y = Math.random() * (innerHeight - radius * 2) + radius;
-      var dx = (Math.random() - 0.5) * 3;
-      var dy = (Math.random() - 0.5) * 3;
+  function init() {
+    circleArray = [];
 
-      //the array pushes new circle each time it refreshes
-      circleArray.push(new Circle(x, y, dx, dy, radius));
-      //var circle = new Circle(200, 200, 3, 3, 30);
+    for (var i = 0; i < 50; i++) {
+        var radius = Math.random() * 3 + 1;
+        var x = Math.random() * (innerWidth - radius * 2) + radius;
+        var y = Math.random() * (innerHeight - radius * 2) + radius;
+        var dx = (Math.random() - 0.5);
+        var dy = (Math.random() - 0.5);
+        //the array pushes new circle each time it refreshes
+        circleArray.push(new Circle(x, y, dx, dy, radius));
+        //var circle = new Circle(200, 200, 3, 3, 30);
+    }
   }
-
-
-
 
   function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
 
     for (var i = 0; i < circleArray.length; i++) {
-      circleArray[i].update();
+        circleArray[i].update();
     }
-
-    //circle.update();
-
-
-    // c.beginPath();
-    // c.arc(x, y, radius, 0, Math.PI * 2, false);
-    // c.strokeStyle = 'blue';
-    // c.stroke();
-
-    // if (x + radius > innerWidth || x - radius < 0) {
-    //   dx = -dx;
-    // }
-    // if (y + radius > innerHeight || y - radius < 0) {
-    //   dy = -dy;
-    // }
-    //
-    // x += dx;
-    // y += dy;
   }
-  animate();
+
+  init();
